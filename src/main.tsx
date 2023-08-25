fc4707748c1c285ddf8a60bf83af63eec5b8c54d
DatDo <thaehan0611@gmail.com>
2023-08-25 10:12:16 +0700

    Update icon and title

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from './App'
-import Flex from 'Components/Flex'
-import Home from 'Screen/Home'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Flex />
-    <Home />
     <App />
   </React.StrictMode>,
 )
